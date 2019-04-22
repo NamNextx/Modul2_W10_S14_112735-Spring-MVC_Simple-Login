8b307fd1c56cfc296068292bcd79556a0516e10f
Namnextx <phuongnam.nextx@gmail.com>
2019-04-22 11:03:48 +0700

    delete packate: mode

--- src/main/java/com/codegym/mode/Login.java
+++
@@ -1,4 +0,0 @@
-package com.codegym.mode;
-
-public class Login {
-}
--- src/main/java/com/codegym/model/User.java
+++ src/main/java/com/codegym/model/User.java
@@ -1,4 +1,64 @@
 package com.codegym.model;
 
+import sun.security.util.Password;
+
 public class User {
+
+   // account, password, name, email, age.
+     private String account;
+     private Password password;
+     private String name;
+     private String email;
+     private int age;
+
+    public User() {
+    }
+
+    public User(String account, Password password, String name, String email, int age) {
+        this.account = account;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
